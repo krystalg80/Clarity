fbbb2dbb09275f5488b5cc3889e1ebb0be0156af
krystalg80 <krystalsoccer80@gmail.com>
2025-01-08 22:15:58 -0800

    users migrations/seeders/model added

---
+++ backend/db/models/user.js
@@ -0,0 +1,73 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notNull: { msg: "Please provide a username" },
+        notEmpty: { msg: "Username cannot be empty" },
+      },
+    },
+    email: { 
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notNull: { msg: "Please provide an email" },
+        notEmpty: { msg: "Email cannot be empty" },
+        isEmail: { msg: "Please provide a valid email" },
+      },
+     },
+    hashedPassword: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Please provide a password" },
+        notEmpty: { msg: "Password cannot be empty" },
+      },
+    },
+    exerciseGoalMinutes: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        isInt: { msg: "Exercise goal must be an integer" },
+        min: { args: [0], msg: "Exercise goal must be greater than or equal to 0" },
+      },
+    },
+    waterGoalOz: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        isInt: { msg: "Water goal must be an integer" },
+        min: { args: [0], msg: "Water goal must be greater than or equal to 0" },
+      },
+    },
+    meditationGoalMinutes: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        isInt: { msg: "Meditation goal must be an integer" },
+        min: { args: [0], msg: "Meditation goal must be greater than or equal to 0" },
+      },
+  },
+ }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
---
+++ backend/db/seeders/20250109045939-demo-user.js
@@ -0,0 +1,56 @@
+'use strict';
+const { query } = require('express');
+const { User } = require('../models');
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await User.bulkCreate([
+      {
+        email: 'demo@user.io',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password'),
+        exerciseGoalMinutes: 30,
+        waterGoalOz: 124,
+        meditationGoalMinutes: 10,
+      },
+      {
+        email: 'user1@user.io',
+        username: 'FakeUser1',
+        hashedPassword: bcrypt.hashSync('password'),
+        exerciseGoalMinutes: 60,
+        waterGoalOz: 64,
+        meditationGoalMinutes: 10,
+      },
+      {
+        email: 'user2@user.io',
+        username: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync('password'),
+        exerciseGoalMinutes: 45,
+        waterGoalOz: 64,
+        meditationGoalMinutes: 10,
+      },
+    ], { validate: true });
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+    }, {});
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
