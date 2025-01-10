01830deaa9cd3d3fcc39d70673da345378f8c7cf
krystalg80 <krystalsoccer80@gmail.com>
2025-01-09 18:53:27 -0800

    workout migrations/seeders/models done

---
+++ backend/db/migrations/20250110023622-create-workout.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Workouts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Users', key: 'id' }
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      durationMinutes: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Workouts');
+  }
+};
---
+++ backend/db/models/workout.js
@@ -0,0 +1,49 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Workout extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Workout.belongsTo(models.User, { foreignKey: 'userId' });
+    }
+  }
+  Workout.init({
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Please provide a user ID" },
+        notEmpty: { msg: "User ID cannot be empty" },
+      },
+    },
+    date: { 
+      type: DataTypes.DATE,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Please provide a date" },
+        notEmpty: { msg: "Date cannot be empty" },
+        isDate: { msg: "Must be a valid date" }
+      },
+    },
+    durationMinutes: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Please provide a duration" },
+        notEmpty: { msg: "Duration cannot be empty" },
+        isInt: { msg: "Duration must be an integer" },
+        min: { args: [1], msg: "Duration must be greater than 0" }
+      },
+    },
+  }, {
+    sequelize,
+    modelName: 'Workout',
+  });
+  return Workout;
+};
