2bf570b0f6041c4f5e0e1cb74b0084f9f1911d91
krystalg80 <krystalsoccer80@gmail.com>
2025-01-14 21:34:35 -0800

    pretty much finished nav bar css

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -10,21 +10,31 @@ function Navigation() {
       <div className="top-bar">
         <div className="logo-container">
           <img src={logo} alt="Clarity logo" className="logo" />
-          <span className="logo-text">Your Wellness Tracker</span>
+          <span className="logo-text">Your Mind + Wellness Tracker</span>
         </div>
         <ProfileButton />
       </div>
       <div className="main-content">
-        <div className="side-bar">
-          <NavLink to="/dashboard" className="nav-link">Dashboard</NavLink>
-          <NavLink to="/workouts" className="nav-link">Workouts</NavLink>
-          <NavLink to="/meditations" className="nav-link">Meditations</NavLink>
-          <NavLink to="/waterintake" className="nav-link">Water Intake</NavLink>
-          <NavLink to="/profile" className="nav-link">Profile</NavLink>
-        </div>
-        <div className="content">
-            <Outlet />
-        </div>
+        <nav className="side-bar">
+          <NavLink to="/dashboard" className={({isActive}) => `nav-link ${isActive ? 'active' : ''}`}>
+            Dashboard
+          </NavLink>
+          <NavLink to="/workouts" className={({isActive}) => `nav-link ${isActive ? 'active' : ''}`}>
+            Workouts
+          </NavLink>
+          <NavLink to="/meditations" className={({isActive}) => `nav-link ${isActive ? 'active' : ''}`}>
+            Meditations
+          </NavLink>
+          <NavLink to="/waterintake" className={({isActive}) => `nav-link ${isActive ? 'active' : ''}`}>
+            Water Intake
+          </NavLink>
+          <NavLink to="/profile" className={({isActive}) => `nav-link ${isActive ? 'active' : ''}`}>
+            Profile
+          </NavLink>
+        </nav>
+        <main className="content">
+          <Outlet />
+        </main>
       </div>
     </div>
   );
