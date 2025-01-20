a18dfa16e8e1bea47ebd66caff3fa937ebb7a2ce
krystalg80 <krystalsoccer80@gmail.com>
2025-01-20 09:08:58 -0800

    tryin new app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import  { useState, useEffect } from 'react';
+import  { useState, useEffect, Outlet } from 'react';
 import { useDispatch } from 'react-redux';
-import { createBrowserRouter, RouterProvider,  Navigate, useLocation } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider,  Navigate, useLocation, Outlet } from 'react-router-dom';
 import WelcomePage from './components/Welcome/WelcomePage';
 import Dashboard from './components/Dashboard/Dashboard';
 import Navigation from './components/Navigation/Navigation';
@@ -14,25 +14,20 @@ function Layout() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
   const location = useLocation();
-  const user = useSelector((state) => state.session.user);
 
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
-  if (!isLoaded) return null;  // You can show a loading spinner here while waiting for the user to be loaded
-
-  // Redirect to /dashboard if the user is authenticated
-  if (user && location.pathname !== '/dashboard') {
-    return <Navigate to="/dashboard" replace />;
-  }
-
-  // Show /welcome if the user is not authenticated
   return (
-    <div className="app-container">
-      {location.pathname !== '/welcome' && <Navigation />}
-      <WelcomePage />
-    </div>
+    <>
+      {isLoaded && (
+        <div className="app-container">
+          {location.pathname !== '/welcome' && <Navigation />}
+          <Outlet />
+        </div>
+      )}
+    </>
   );
 }
 
@@ -42,7 +37,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <Navigate to="/welcome" replace />,
+        element: <Navigate to="/welcome" />,
       },
       {
         path: '/welcome',
